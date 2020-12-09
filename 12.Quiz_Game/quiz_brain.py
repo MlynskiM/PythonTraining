730a6aecf1c845339dc4cba59ac16e63d8ec8cf2
Maks <mlynskimax@gmail.com>
2020-12-09 19:28:47 +0100

    13 project Hirts Painting

--- 12.Quiz_Game/quiz_brain.py
+++ 12.Quiz_Game/quiz_brain.py
@@ -6,23 +6,25 @@ class QuizBrain:
         self.score = 0
 
     def still_has_questions(self):
-         return self.question_number < len(self.questions_list)
+        """Checking question availability returning True or False"""
+        return self.question_number < len(self.questions_list)
         
        
 
     def next_question(self):
+        """Printing question from the list in range 1 to len(list)"""
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
-        player_answer = input(f"Q.{self.question_number}{current_question.text} (True/False): ")
+        player_answer = input(f"Q.{self.question_number} {current_question.text} (True/False): ")
         self.check_answer(player_answer, current_question.answer)
         
 
     
     def check_answer(self, user_answer, correct_answer):
+        """Checking answer is it correct or not?"""
         if user_answer.lower() == correct_answer.lower():
             print("You got it right!")
             self.score += 1
-
         else:
             print("That's wrong.")
         print(f"The correct answer was: {correct_answer}.")
